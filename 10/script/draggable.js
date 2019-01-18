81a741c24c229f772a08fe5602282eae193fe9d7
Theia <theia.flynn@gmail.com>
2019-01-18 13:02:16 -0500

    Adding my boring

---
+++ 10/script/draggable.js
@@ -0,0 +1,19 @@
+
+$(function() {
+    $( "#draggable" ).draggable();
+    $( "#draggable1" ).draggable();
+    $( "#draggable2" ).draggable();
+    $( "#draggable3" ).draggable();
+    $( "#draggable4" ).draggable();
+    $( "#draggable5" ).draggable();
+    $( "#draggable6" ).draggable();
+    $( "#draggable7" ).draggable();
+    $( "#draggable8" ).draggable();
+    $( "#draggable9" ).draggable();
+    $( "#draggable10" ).draggable();
+  });
+
+
+
+
+
